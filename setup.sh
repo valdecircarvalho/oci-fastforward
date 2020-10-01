970e73f464543c72c0e0960155156232c4338349
valdecircarvalho <valdecir.carvalho@outlook.com>
2020-10-01 17:26:11 -0300

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -26,21 +26,21 @@ echo -e "$Yellow \n Starting... $Color_Off"
 # System Update
 echo -e "$Purple \n Initializing System Updates... $Color_Off"
 
-sudo yum update -y 2>&1 >setup.log
+sudo yum update -y 2>&1 > setup.log
 
 # Disable Firewall
 echo -e "$Purple \n Initializing System Setup... $Color_Off"
-sudo systemctl stop firewalld 2>&1 >setup.log
-sudo systemctl disable firewalld 2>&1 >setup.log
-sudo systemctl mask firewalld 2>&1 >setup.log
+sudo systemctl stop firewalld 2>&1 > setup.log
+sudo systemctl disable firewalld 2>&1 > setup.log
+sudo systemctl mask firewalld 2>&1 > setup.log
 
 # Install Tools
 echo -e "$Purple \n Initializing Tools Instalation... $Color_Off"
-sudo yum install git -y  2>&1 >setup.log
-cd /home/opc 2>&1 >setup.log
-git clone https://github.com/valdecircarvalho/oci-fastforward.git 2>&1 >setup.log
-cd /home/opc/oci-fastforward 2>&1 >setup.log
-clear 2>&1 >setup.log
+sudo yum install git -y  2>&1 > setup.log
+cd /home/opc 2>&1 > setup.log
+git clone https://github.com/valdecircarvalho/oci-fastforward.git 2>&1 > setup.log
+cd /home/opc/oci-fastforward 
+
 
 # Finishing
 echo -e "$Yellow \n Finishing... $Color_Off"
