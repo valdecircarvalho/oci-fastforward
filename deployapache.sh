146a3b9d98cdd46aff5ac44d5ed2115cb79ef940
valdecircarvalho <valdecir.carvalho@outlook.com>
2020-10-01 16:55:04 -0300

    install apache code fixes

--- deployapache.sh
+++ deployapache.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 #######################################
 # Bash scritp to install Apache web server and deploy simple web page
@@ -18,33 +18,26 @@ Yellow='\033[0;33m'       # Yellow
 Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 
-# Update packages and Upgrade system
-echo -e "$Cyan \n Updating System.. $Color_Off"
-sudo yum update -y 
+clear
 
-## Install AMP
-echo -e "$Cyan \n Installing Apache2 $Color_Off"
-sudo yum install apache2 apache2-doc apache2-mpm-prefork apache2-utils libexpat1 ssl-cert -y php5 -y
+# Initializing apache deploy
+echo -e "$Purple \n Initializing Apache deploy... $Color_Off"
 
-echo -e "$Cyan \n Verifying installs$Color_Off"
-sudo yum install apache2 libapache2-mod-php5  -y
-
-## TWEAKS and Settings
-# Permissions
-echo -e "$Cyan \n Permissions for /var/www $Color_Off"
-sudo chown -R www-data:www-data /var/www
-echo -e "$Green \n Permissions have been set $Color_Off"
+## Install Apache
+echo -e "$Cyan \n Installing Apache Server... $Color_Off"
+sudo yum install httpd php -y 2>&1 >deployapache.log
 
 # Restart Apache
-echo -e "$Cyan \n Restarting Apache $Color_Off"
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
-# Configure Firewall
-echo -e "$Cyan \n Configuring firewall $Color_Off"
-sudo firewall-cmd --permanent --zone=public --add-port=80/tcp --add-port=443/tcp
-sudo firewall-cmd ––reload
+echo -e "$Cyan \n Restarting Apache Server... $Color_Off"
+sudo systemctl start httpd 2>&1 >deployapache.log
+sudo systemctl enable httpd 2>&1 >deployapache.log
 
 # Deploy sample website
-echo -e "$Cyan \n Deploying sample website $Color_Off"
-sudo cp /home/opc/oci-fastforward/server-info.php /var/www/html/index.php
+echo -e "$Cyan \n Deploying sample website... $Color_Off"
+sudo cp /home/opc/oci-fastforward/server-info.php /var/www/html/index.php 2>&1 >deployapache.log
+
+clear 2>&1 >deployapache.log
+sleep 5 2>&1 >deployapache.log
+
+# Deploy finalizado
+echo -e "$Yellow \n Deploy finished. $Color_Off"
